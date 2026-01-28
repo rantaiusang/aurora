661efac3d04f0e3d8adc2dfd753ef3febed4df62
rantaiusang <baramultijavapalma@gmail.com>
2026-01-28 19:46:05 +0700

    Update pi-auth.js

--- public/js/pi-auth.js
+++ public/js/pi-auth.js
@@ -1,9 +1,22 @@
+// pi-auth.js
 async function startPiAuth() {
   if (!initPiSDK()) {
-    throw new Error("Pi SDK gagal init");
+    throw new Error("Pi SDK gagal diinisialisasi. Pastikan tidak ada pemblokir iklan.");
   }
 
-  return Pi.authenticate(['username'], () => {});
+  // Melakukan autentikasi Pi Network
+  // Callback kosong () => {} dibolehkan jika kita handle via Promise
+  try {
+    const authResult = await Pi.authenticate(['username'], {
+      onIncompletePaymentFound: (payment) => { 
+        // Opsional: Handle jika ada pembayaran tertunda
+        console.log("Pembayaran tertunda ditemukan", payment); 
+      }
+    });
+    return authResult;
+  } catch (err) {
+    throw new Error("Otentikasi Pi ditolak oleh user: " + err.message);
+  }
 }
 
 window.startPiAuth = startPiAuth;
