a79a94504f9b3bff0aed517dbd18c57318d99b41
rantaiusang <baramultijavapalma@gmail.com>
2026-01-28 19:43:25 +0700

    Update pi-config.js

--- public/js/pi-config.js
+++ public/js/pi-config.js
@@ -1,6 +1,10 @@
+// pi-config.js
 window.APP_CONFIG = {
   IS_SANDBOX: true,
-  SUPABASE_URL: "https://dikapquhusbwjccbqcsb.supabase.co",
-  FUNCTION_NAME: "https://dikapquhusbwjccbqcsb.supabase.co/functions/v1/verify-pi-login",
+  
+  // URL Backend Supabase Edge Function (Langsung Full URL)
+  API_URL: "https://dikapquhusbwjccbqcsb.supabase.co/functions/v1/verify-pi-login",
+  
+  // Supabase Anon Key untuk otentikasi ke fungsi backend
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpa2FwcXVodXNid2pjY2JxY3NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxNjQ1NTQsImV4cCI6MjA4NDc0MDU1NH0.X278oHDF0be7oa25484eliSukSYAYvDbJyU6ysz83zA"
 };
